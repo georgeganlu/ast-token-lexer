4bcbc6a69d9d063b669af5676daee419a348a565
文军辉 <wenjunhui1219@163.com>
2020-05-14 09:25:50 +0800

    新增browser的部分

--- test07-browser/abbott-master/examples/example.js
+++ test07-browser/abbott-master/examples/example.js
@@ -8,14 +8,16 @@ var buildLayoutTree= require("../src/boxes.js")
 var html = "<html><body><h1>Title</h1><div id='main' class='test'><p>Hello<em>world</em>!</p></div></body></html>"
 var dom = parseHTML(html)
 
-console.log(util.inspect(dom, false, null));
+// console.log(util.inspect(dom, false, null));
 
 // CSSOM
 var css = "h1, h2, h3 { left: auto; margin: auto; color: #cc0000; } div.note { margin-bottom: 20px; padding: 10px; } #answer { display: none; }"
 var cssNodes = parseCSS(css) 
+console.log(cssNodes, 'cssNodes');
+
 // console.log(util.inspect(cssNodes, false, null));
 
 var stree = styleTree(dom, cssNodes)
-// console.log(util.inspect(stree, false, null));
+console.log(util.inspect(stree, false, null));
 
 // console.log(buildLayoutTree(stree))
