4230b62e8f619fa16ae559048699590e3310d456
georgeganlu <wenjunhui1219@163.com>
2020-05-28 08:34:55 +0800

    asd

---
+++ test09-class/test2-object.js
@@ -0,0 +1,23 @@
+// js所有的对象。
+
+// 宿主对象 / 内置对象。
+// 内置对象里面包函的 固有对象和原生对象。
+
+// 对于对象来说可以是 函数对象和构造函数，在js中 原生对象是能通过构造器来创建新对象的。 内置对象承载的是函数类型的对象 eg: encodeURLComponent
+/**  
+ *   原生对象按类型来区分的话。
+ *   基本数据类型       基础功能和数据结构        错误类型         二进制操作         带类型的数组    
+ *   String            Promise
+ *   Number            Array
+ *   Boolean           RegExp
+ *   Object            Date
+ *   Symbol            Function
+ *                     Map
+ *                     weakMap
+ *                     Set
+ *                     weakSet
+ *                     Proxy 
+ *                      
+ * 
+ * */ 
+
